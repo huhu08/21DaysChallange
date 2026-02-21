f3213ec975e46f2603f69f1e165d1ae58b2ecb28
Huda <hudaelzubair@hotmail.com>
2026-02-21 11:00:21 +0300

    1st commit:task struct

---
+++ task_manager/src/main.rs
@@ -0,0 +1,27 @@
+struct Task{
+    id:u32,
+    title :string,
+    description:string,
+    completed:bool
+}
+
+
+fn main() {
+   let mut task:vec<Task>=vec::new();
+   loop {
+        println!("press 1 to add task");
+        println!("press 2 to edit task");
+        println!("press 3 to list all tasks");
+        println!("press 4 to delete task");
+        println!("press 5 to mark task completed");
+
+
+ 
+    }
+fn getInput(input &string)->String {
+
+}
+
+
+}
+
